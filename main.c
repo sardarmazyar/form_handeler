b2079f3bc1c0c6239785b0800a9925c55692b96a
sardarmazyar <sardar.mazyar@gmail.com>
2022-07-10 18:20:15 +0430

    last main.c

--- main.c
+++ main.c
@@ -3,8 +3,12 @@
 #include <dirent.h>
 #include <string.h>
 #include <stdlib.h>
+#include <curses.h>
+
+
 #define form_len 1000
 #define name_len 100
+
 char **list_forms;
 int n_forms=0;
 void read_forms(){
@@ -14,35 +18,40 @@ void read_forms(){
     struct dirent *dp;
     DIR *dir = opendir(".");
     if (!dir)
-        printf("we couldn't open the directory\n");
+        printw("we couldn't open the directory\n");
     while ((dp = readdir(dir)) != NULL){
-        if (!strcmp(dp->d_name+ dp->d_namlen-4,".txt"))
+        int len=strlen(dp->d_name);
+        if (!strcmp(dp->d_name+len-4,".txt"))
             strcpy(list_forms[n_forms++], dp->d_name);
     }
     closedir(dir);
 }
 int print_forms(){
     for (int i=0;i<n_forms;i++)
-        printf("%s\n",list_forms[i]);
+        printw("%s\n",list_forms[i]);
     return 1;
 }
 int new_form(){
-    printf("choose a name form your form:\n");
+    printw("choose a name form your form:\n");
+    refresh();
+    echo();
     char name[100];
-    scanf("%[^\n]s",name);
-    getchar();
+    wgetstr(stdscr,name);
+    //getchar();
     strcat(name,".txt");
     for (int i=0;i<n_forms;i++)
         if (!strcmp(name,list_forms[i])){
             char c;
             char order[100];
-            printf("a file with this name already exists. do want to \"choose another name\" or get \"back to main menu\"\n");
-            scanf("%[^\n]s",order);
-            getchar(); // \n
+            printw("a file with this name already exists. do want to \"choose another name\" or get \"back to main menu\"\n");
+            refresh();
+            wgetstr(stdscr,order);
+            //getchar(); // \n
             while (strcmp(order,"choose another name") && strcmp(order,"back to main menu")){
-                printf("wrong input. insert 'choose another name' to choose an other name or insert 'back to main menu' to get back to the main_menu?\n");
-                scanf("%[^\n]s",order);
-                getchar(); // \n
+                printw("wrong input. insert 'choose another name' to choose an other name or insert 'back to main menu' to get back to the main_menu?\n");
+                refresh();
+                wgetstr(stdscr,order);
+                //getchar(); // \n
             }
             if (!strcmp(order,"choose another name"))
                 return new_form();
@@ -51,16 +60,24 @@ int new_form(){
         }
     FILE *fp;
     fp= fopen(name,"w");
-    name[strlen(name) -4 ]='\0';
+    int len= strlen(name);
+    name[len -4 ]='\0';
     mkdir(name);
     fclose(fp);
+    name[len -4 ]='.';
     strcpy(list_forms[n_forms++],name);
+    printw("form created sucsessfully\n");
+    refresh();
     return 1;
 }
+
 int main_menu(){
+
+    printw("you are in main menu\n");
+    refresh();
     char order[20];
-    scanf("%[^\n]s",order);
-    getchar();
+    wgetstr(stdscr,order);
+    //getchar();
     if (!(strcmp(order,"new form")))
         return new_form();
     else if (!(strcmp(order,"list of forms")))
@@ -70,7 +87,13 @@ int main_menu(){
 
 
 int main() {
+    int maxy,maxx;
+
+    initscr();
+    getmaxyx(stdscr,maxy,maxx);
+    move(7*maxy/10,0);
     read_forms();
     while(main_menu());
+    endwin();
     return 0;
 }
