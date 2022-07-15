21ea62bce0d48d1f4ebe68e981a8fe24fdb04cfd
sardarmazyar <96318787+sardarmazyar@users.noreply.github.com>
2022-07-15 17:25:03 +0430

    last update

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <curses.h>
-//#include <conio.h>
+
 #define form_len 1000
 #define name_len 100
 
@@ -37,8 +37,8 @@ void read_form(FILE *fp) {
             box(win_out[i], str2[0], str2[0]);
         wrefresh(win_out[i]);
         fgetc(fp);
-        if (!(strcmp(str, "button")) || !(strcmp(str, "label"))){
-            fscanf(fp, "%s", str);
+        if (!(strcmp(str, "button")) || !(strcmp(str, "label"))) {
+            fscanf(fp, "%[^\n]s", str);
             fgetc(fp);
             wprintw(win_in[i], "%s\n", str);
             wrefresh(win_in[i]);
@@ -50,17 +50,16 @@ void read_form(FILE *fp) {
                 fscanf(fp, "%s", str);
                 fgetc(fp);
             }
-        } else if (!(strcmp(str, "checkbox"))){
+        } else if (!(strcmp(str, "checkbox"))) {
             fscanf(fp, "%d", &n);
             fgetc(fp);// \n
             for (int j = 0; j < n; j++) {
                 fscanf(fp, "%s", str);
                 fgetc(fp);
-                wprintw(win_in[i],"%s\n",str);
+                wprintw(win_in[i], "%s\n", str);
             }
         }
     }
-    getch();
 }
 
 void read_forms(WINDOW *command_win) {
@@ -82,73 +81,82 @@ void read_forms(WINDOW *command_win) {
 int print_forms(WINDOW *command_win) {
     for (int i = 0; i < n_forms; i++)
         wprintw(command_win, "%s\t", list_forms[i]);
+    wrefresh(command_win);
     wprintw(command_win, "\n");
     return 1;
 }
 
 int design_form(WINDOW *command_win, FILE *fp) {
+    wclear(command_win);
     read_form(fp);
     char order[name_len];
     wprintw(command_win, "insert your order:\n");
     wgetstr(command_win, order);
-    int border_info[9],n;
+    int border_info[9], n;
     char border_type[name_len];
     while (strcmp(order, "end")) {
         fseek(fp, 0, SEEK_SET);
         fscanf(fp, "%d", &n);
+        fseek(fp, 0, SEEK_SET);
         fprintf(fp, "%d", n + 1);
         fseek(fp, 0, SEEK_END);
         wscanw(command_win, "%d", &border_info[0]);
         if (border_info[0]) {
-            wscanw(command_win, "%d %d %d %d %d %d %d %d", &border_info[1], &border_info[2], &border_info[3],
-                   &border_info[4], &border_info[5], &border_info[6], &border_info[7], &border_info[8]);
-            fprintf(fp, "%d %d %d %d %d %d %d %d", border_info[1], border_info[2], border_info[3], border_info[4],
+            for (int i = 1; i < 9; i++) {
+                wclear(command_win);
+                wscanw(command_win, "%d", &border_info[i]);
+            }
+            fprintf(fp, "%s %d %d %d %d %d %d %d %d %d ", order, border_info[0], border_info[1], border_info[2],
+                    border_info[3], border_info[4],
                     border_info[5], border_info[6], border_info[7], border_info[8]);
         } else {
-            wscanw(command_win, "%d %d %d %d", border_info[1], border_info[2], border_info[3], border_info[4]);
-            fprintf(fp, "%d %d %d %d", border_info[1], border_info[2], border_info[3], border_info[4]);
+            for (int i = 1; i < 5; i++) {
+                wclear(command_win);
+                wscanw(command_win, "%d", &border_info[i]);
+            }
+            fprintf(fp, "%s %d %d %d %d %d ", order, border_info[0], border_info[1], border_info[2], border_info[3],
+                    border_info[4]);
         }
+        wclear(command_win);
         wscanw(command_win, "%s", border_type);
         fprintf(fp, "%s\n", border_type);
         if (!strcmp(order, "label")) {
             char label_text[name_len];
             wprintw(command_win, "insert the text inside the label:\n");
-            wscanw(command_win, "%s", label_text);
+            wgetstr(command_win, label_text);
             fprintf(fp, "%s\n", label_text);
         } else if (!strcmp(order, "button")) {
             char button[name_len];
             wprintw(command_win, "insert the text inside the button:\n");
-            wscanw(command_win, "%s", button);
+            wgetstr(command_win, button);
             fprintf(fp, "%s\n", button);
-            wprintw(command_win, "insert your order:\n");
-            wgetstr(command_win, order);
         }
+        fseek(fp, 0, SEEK_SET);
+        read_form(fp);
+        wprintw(command_win, "insert your order:\n");
+        wgetstr(command_win, order);
     }
     return 1;
 }
 
 int new_form(WINDOW *command_win) {
-    wprintw(command_win, "choose a name form your form:\n");
-    //refresh();
-    //echo();
+    wprintw(command_win, "choose a name for your form:\n");
     char name[name_len];
     wgetstr(command_win, name);
-    //getchar();
     strcat(name, ".txt");
     for (int i = 0; i < n_forms; i++)
         if (!strcmp(name, list_forms[i])) {
             char order[100];
             wprintw(command_win,
                     "a file with this name already exists. do want to \"choose another name\" or get \"back to main menu\"\n");
-            refresh();
-            wgetstr(stdscr, order);
-            //getchar(); // \n
+            wgetstr(command_win, order);
+            wclear(command_win);
             while (strcmp(order, "choose another name") && strcmp(order, "back to main menu")) {
                 wprintw(command_win,
                         "wrong input. insert 'choose another name' to choose an other name or insert 'back to main menu' to get back to the main_menu?\n");
                 refresh();
-                wgetstr(stdscr, order);
-                //getchar(); // \n
+                wgetstr(command_win, order);
+                wclear(command_win);
             }
             if (!strcmp(order, "choose another name"))
                 return new_form(command_win);
@@ -161,35 +169,44 @@ int new_form(WINDOW *command_win) {
     name[len - 4] = '\0';
     mkdir(name);
     fprintf(fp, "%d\n", 1);
-    wprintw(command_win, "insert the border information:\n");
+    wclear(command_win);
+    wprintw(command_win, "insert border information:\n");
     int border_info[10];
     wscanw(command_win, "%d", &border_info[0]);
     if (!border_info[0]) {
-        wscanw(command_win, "%d %d %d %d %c", &border_info[1], &border_info[2], &border_info[3], &border_info[4],
-               &border_info[5]);
-        fprintf(fp, "border %d %d %d %d %c\n", border_info[1], border_info[2], border_info[3], border_info[4],
-                border_info[5]);
+        // wprintw(command_win,"Im in");
+        for (int i = 1; i < 5; i++) {
+            wscanw(command_win, "%d", &border_info[i]);
+            wclear(command_win);
+        }
+            fprintf(fp, "border %d %d %d %d %d ", border_info[0], border_info[1], border_info[2], border_info[3],
+                border_info[4]);
     } else {
-        wscanw(command_win, "%d %d %d %d %d %d %d %d %c", &border_info[1], &border_info[2], &border_info[3],
-               &border_info[4], &border_info[5], &border_info[6], &border_info[7], &border_info[8], &border_info[9]);
-        fprintf(fp, "border %d %d %d %d %d %d %d %d %c\n", border_info[1], border_info[2], border_info[3],
-                border_info[4], border_info[5], border_info[6], border_info[7], border_info[8], border_info[9]);
+        for (int i = 1; i < 9; i++) {
+            wscanw(command_win, "%d", &border_info[i]);
+            wclear(command_win);
+        }
+            fprintf(fp, "border %d %d %d %d %d %d %d %d %d ", border_info[0], border_info[1], border_info[2],
+                border_info[3],
+                border_info[4], border_info[5], border_info[6], border_info[7], border_info[8]);
     }
+    char border_type[name_len];
+    wscanw(command_win, "%s", border_type);
+    fprintf(fp, "%s\n", border_type);
     name[len - 4] = '.';
     strcpy(list_forms[n_forms++], name);
     wprintw(command_win, "form created successfully\n");
-    //refresh();
+    fclose(fp);
+    fp = fopen(name, "r+");
     return design_form(command_win, fp);
 }
 
 
-int main_menu(WINDOW *command_win, int maxy, int maxx) {
-    //system("clear");
+int main_menu(WINDOW *command_win) {
     wprintw(command_win, "you are in main menu\n");
     wrefresh(command_win);
-    char order[20];
+    char order[name_len];
     wgetstr(command_win, order);
-    //getchar();
     wclear(command_win);
     if (!(strcmp(order, "new form")))
         return new_form(command_win);
@@ -204,10 +221,11 @@ int main_menu(WINDOW *command_win, int maxy, int maxx) {
             return 1;
         }
         return design_form(command_win, fp);
-    } else if (!strcmp(order, "exit"))
+    }
+    else if (!strcmp(order, "exit"))
         return 0;
     else {
-        wprintw(command_win, "command \"%s\" not found, insert again\n");
+        wprintw(command_win, "command \"%s\" not found, insert again\n",order);
         return 1;
     }
 }
@@ -216,14 +234,11 @@ int main_menu(WINDOW *command_win, int maxy, int maxx) {
 int main() {
     int maxy, maxx;
     initscr();
-//    FILE *fp = fopen("wert.txt","r+");
-//    read_form(fp);
     getmaxyx(stdscr, maxy, maxx);
     WINDOW *command_win = newwin(maxy - (8 * maxy / 10), maxx, 8 * maxy / 10, 0);
     refresh();
     read_forms(command_win);
-    while (main_menu(command_win, maxy, maxx));
-    wprintw(command_win, "Good bye\n");
+    while (main_menu(command_win));
     endwin();
     return 0;
 }
